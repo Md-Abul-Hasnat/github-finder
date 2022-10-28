864d8656fda09eb7cebc5e0977016631b2d5249b
Hasnat <webdevhasnat@gmail.com>
2022-10-28 18:47:27 +0600

    done

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import "./footer.css";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <small> All rights reserved by WebDev Hasnat </small>
+      <small>
+        {" "}
+        Made with <FontAwesomeIcon className="logo-icon" icon={faHeart} /> by
+        WebDev Hasnat{" "}
+      </small>
     </footer>
   );
 };
